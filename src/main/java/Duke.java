845e31aa3befed8bf7076bf2b1e75cc13e4ef29f
mxksowie <34589654+mxksowie@users.noreply.github.com>
2020-08-30 17:51:48 +0800

    Add greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
 public class Duke {
+    /**
+     * Main method which (strictly) handles IO
+     * @param args
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +10,31 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        printBreak();
+        greetUser();
+
+        printBreak();
+        byeUser();
+        
+        printBreak();
+
+
+    }
+
+    /**
+     * Prints a standard breakline meant to break up sections of text
+     */
+    private static void printBreak() {
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void greetUser() {
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+    }
+
+    private static void byeUser() {
+        System.out.println(" Bye. Hope to see you again soon!");
     }
 }
